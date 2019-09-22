776baf457a269a8c6dd3fb7075b3a0222cbe44f2
Sergey <pmswga@gmail.com>
2019-09-22 13:02:54 +0300

    fix first program

--- main.java
+++ main.java
@@ -4,13 +4,30 @@ class MyPerson {
     private String initials;
     private String group;
 
+    MyPerson() {
+        this.initials = "";
+        this.group    = "";   
+    }
+
+    MyPerson(String initials) {
+        this.initials = initials;
+    }
+
     MyPerson(String initials, String group) {
         this.initials = initials;
-        this.group = group;
+        this.group    = group;
     }
     
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
     public String getInitials() {
         return this.initials;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
     }
 
     public String getGroup() {
